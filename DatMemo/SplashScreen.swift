76adcb39161b300842038c7d050526c20dc38b00
Gramatin Artiom <121014424+GramatinArtiom@users.noreply.github.com>
2024-04-25 12:11:28 +0300

    SplashScreen
    Added a SplashScreen

---
+++ DatMemo/SplashScreen.swift
@@ -0,0 +1,46 @@
+//SplashScreen
+//
+//
+//
+import SwiftUI
+
+struct SplashScreen: View{
+    @State var isActive: Bool = false
+    let background = Image("Background")
+    var body: some View{
+        ZStack {
+            
+            if self.isActive {
+                ContentView()
+            }
+            else {
+                background
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+            VStack{
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .imageScale(.small)
+                    .containerRelativeFrame(.horizontal){
+                        size, axis in size * 0.5
+                    }
+            }
+            }
+        }
+        .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        withAnimation {
+                            self.isActive = true
+                        }
+                    }
+                }
+
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
+
